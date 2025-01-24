d5958fb44d0db5ca831d4420260da3b4b327716e
Yehor <156694343+lxstik@users.noreply.github.com>
2025-01-24 15:09:09 +0000

    123

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Tabla } from './Tabla.jsx'
+import Juego from './vistas/Juego.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Juego/>
     <Tabla/>
   </StrictMode>,
 )
